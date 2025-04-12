d66995472b351a8887351aa0a125e3809e8f7620
AlexCoder13 <sesasha99@gmail.com>
2025-04-13 01:03:10 +0300

    new fixes and updates

---
+++ Movies App/Movies-3rd-Challenge 8/Support/SceneDelegate.swift
@@ -0,0 +1,45 @@
+//
+//  SceneDelegate.swift
+//  Movies-3rd-Challenge
+//
+//  Created by Валентин Картошкин on 28.03.2025.
+//
+
+import UIKit
+import GoogleSignIn
+import FirebaseAuth
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        
+        let auth = UserDefaults.standard.bool(forKey: "isAuth")
+//        let auth = true
+        
+        if let theme = UserDefaults.standard.string(forKey: "AppTheme") {
+            switch theme {
+            case "dark":
+                window?.overrideUserInterfaceStyle = .dark
+            case "light":
+                window?.overrideUserInterfaceStyle = .light
+            default:
+                window?.overrideUserInterfaceStyle = .unspecified
+            }
+        }
+        window?.rootViewController = UINavigationController(rootViewController: auth ? TabBarController() : LoginVC() )
+        window?.makeKeyAndVisible()
+        
+    }
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        guard let url = URLContexts.first?.url else { return }
+        GIDSignIn.sharedInstance.handle(url)
+    }
+
+  
+}
+
